cba176556eafd977c0350de353eac1a980cce240
V <slvbtv@gmail.com>
2024-02-29 12:56:26 +0500

    Fix: add @Component on BotApplication for properly scheduling

--- bot/src/main/java/edu/java/bot/BotApplication.java
+++ bot/src/main/java/edu/java/bot/BotApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 @EnableConfigurationProperties(ApplicationConfig.class)
+@EnableScheduling
 public class BotApplication {
     public static void main(String[] args) {
         SpringApplication.run(BotApplication.class, args);
--- bot/src/main/java/edu/java/bot/scheduler/LinkUpdaterScheduler.java
+++ bot/src/main/java/edu/java/bot/scheduler/LinkUpdaterScheduler.java
@@ -1,10 +1,14 @@
 package edu.java.bot.scheduler;
 
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+@SuppressWarnings("RegexpSinglelineJava")
+@Component
 public class LinkUpdaterScheduler {
 
     @Scheduled(fixedDelayString = "${app.scheduler.interval}")
     public void update() {
+        System.out.println("SchedulerStab");
     }
 }
